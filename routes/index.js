db10c6c3eade28d577a4fa473f73f25a65de9d6e
Sam Pattuzzi <sam.pattuzzi@gmail.com>
2015-03-17 09:01:30 +0000

    Update the cards and other images.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,33 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', challenges: [
+    {
+      title: "Wake up",
+      id: "wake-up-challenge",
+      image: "card1.png",
+      members: "1,056",
+      success: "91%",
+      days: "7"
+    },
+    {
+      title: "Get out more",
+      id: "get-out-challenge",
+      image: "card2.png",
+      members: "726",
+      success: "84%",
+      days: "7"
+    },
+    {
+      title: "Run to fit",
+      id: "running-challenge",
+      image: "card3.png",
+      members: "502",
+      success: "81%",
+      days: "7"
+    },
+    ]
+  });
 });
 
 module.exports = router;
